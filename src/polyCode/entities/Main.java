996ed88cde1ea019aaf2a62cff5032187d7b0fd2
Ziga <ziga.sajovic@gmail.com>
2016-03-24 21:02:55 +0100

    Added Main for Applet

---
+++ src/polyCode/entities/Main.java
@@ -0,0 +1,15 @@
+package polyCode.entities;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class Main extends Applet{
+	   /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void paint(Graphics g){
+	      new Display().show();
+	   }
+	}
